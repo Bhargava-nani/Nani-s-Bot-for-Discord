6bbab2afbb917f99bf395de93f55e0618155a03a
Bhargava-nani <matchabhargava07@gmail.com>
2026-05-12 13:35:17 +0530

    Add event handler for invite deletion

--- src/events/inviteDelete.js
+++ src/events/inviteDelete.js
@@ -1,1 +1,15 @@
+import { Events } from 'discord.js';
+import { removeInvite } from '../utils/inviteTracker.js';
 
+export default {
+  name: Events.InviteDelete,
+  once: false,
+
+  async execute(invite) {
+    try {
+      removeInvite(invite.guildId, invite.code);
+    } catch {
+      // ignore
+    }
+  },
+};
